e386d1d6c30c25cf9d6693cc5730352a8921b4ef
Rhys <rhysss@gmail.com>
2019-11-29 02:07:11 -0500

    add display block info, add load button functionality

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Block = ({ blockId }) => {
-  return <div>{blockId}</div>;
+  const blockInfo = useSelector(state => state.eos.blocks[blockId]);
+  return (
+    <div>
+      {blockInfo ? (
+        <div>
+          {blockInfo.timestamp} {blockInfo.id}{" "}
+          {blockInfo.transactions.length + 1}
+        </div>
+      ) : (
+        blockId
+      )}
+    </div>
+  );
 };
 
 export default Block;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
 import RefreshIcon from "./RefreshIcon";
+import { useDispatch } from "react-redux";
+import { fetchInfo } from "../actions";
 
 const Header = () => {
+  const dispatch = useDispatch();
   return (
     <nav className="flex items-center justify-between flex-wrap border-b-2 border-green-600 bg-teal-100 py-2 px-1">
       <div className="w-full block flex-grow flex lg:items-center lg:w-auto">
@@ -9,7 +12,10 @@ const Header = () => {
           <h1 className="uppercase inline text-lg font-bold">EOS.IO blocks</h1>
         </div>
         <div>
-          <button className="inline-block border border-blue-500 bg-transparent hover:bg-blue-700 text-sm font-semibold hover:border-transparent text-blue-700 px-1 py-1 rounded">
+          <button
+            onClick={() => dispatch(fetchInfo())}
+            className="inline-block border border-blue-500 bg-transparent hover:bg-blue-700 text-sm font-semibold hover:border-transparent text-blue-700 px-1 py-1 rounded"
+          >
             <RefreshIcon /> LOAD
           </button>
         </div>
