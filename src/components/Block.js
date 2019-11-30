b811d4c2e662c31e21fb777c5bdaa312df87117a
Rhys <rhysss@gmail.com>
2019-11-30 01:59:38 -0500

    add block item refinement

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,21 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { createSelector } from "@reduxjs/toolkit";
+
 import { fetchBlock } from "../actions";
 
+const actionCountSelector = createSelector();
+//blocks
+
+const countActions = blockInfo => {
+  //return blockInfo.
+  // blockInfo.transactions (is an arr)
+  // [0].trx (either object or string)
+  // .transaction.actions
+};
+
 const Block = ({ blockId }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
   const blockInfo = useSelector(state => state.eos.blocks[blockId]);
+  //const actionCount = useSelector()
   useEffect(() => {
     if (!blockInfo) {
       dispatch(fetchBlock(blockId));
     }
   }, [blockId, blockInfo, dispatch]);
   return (
-    <div>
+    <div className="p-3 border-b-2">
       {blockInfo ? (
-        <div>
-          {blockInfo.timestamp} {blockInfo.id}{" "}
-          {blockInfo.transactions.length + 1}
+        <div
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          <div className="text-sm">{blockInfo.timestamp}</div>
+          <div className="text-lg p-2">{blockInfo.id}</div>
+          <div className="text-lg italic px-2">
+            {blockInfo.transactions.length + 1} actions
+          </div>
+          {isOpen && <div>BLOCKINFO</div>}
         </div>
       ) : (
         blockId
